afa524c90f9ac20635aec8a74ae81043874e3fb1
haneul <haneulgim95@gamil.com>
2026-05-15 00:00:18 +0900

    ThemeContext를 통한 theme 적용

---
+++ src/context/theme/themeContext.ts
@@ -0,0 +1,14 @@
+import { createContext } from "react";
+
+export type ThemeType = "light" | "dark";
+
+
+export type ThemeContextType = {
+    theme: ThemeType;
+    onChangeTheme: VoidFunction;
+}
+
+export const ThemeContext = createContext<ThemeContextType>({
+    theme: "light",
+    onChangeTheme: () => {},
+})
