47cd53d4580c076843bdf69958af0c7447ed5788
DeveloperSky95 <haneulgim95@gmail.com>
2026-05-15 09:15:28 +0900

    signupPage 디자인 완성

--- src/components/common/button/Button.tsx
+++ src/components/common/button/Button.tsx
@@ -4,19 +4,26 @@ import type { ButtonHTMLAttributes, ElementType, ReactNode } from "react";
 type ButtonColorType = "primary" | "secondary" | "success" | "error" | "warning" | "info";
 type VariantType = "contained" | "text" | "icon";
 
-const StyledButton = styled.button<{ $color: ButtonColorType; $variant: VariantType }>`
+const StyledButton = styled.button<{
+    $color: ButtonColorType;
+    $variant: VariantType;
+    $fullwidth: boolean;
+}>`
+    width: ${props => props.$fullwidth ? "100%" : "auto"};
     font-size: 14px;
     font-weight: 600;
     color: ${props => (props.$variant === "contained" ? "#fff" : props.theme.colors.text.default)};
-    background-color: ${props => props.$variant === "contained" ? props.theme.colors[props.$color] : "transparent"};
-    padding: ${props => props.$variant === "icon" ? "8px" : "8px 16px"};
-    border-radius: ${props => props.$variant === "icon" ? "50%" : "6px"};
+    background-color: ${props =>
+        props.$variant === "contained" ? props.theme.colors[props.$color] : "transparent"};
+    padding: ${props => (props.$variant === "icon" ? "8px" : "8px 16px")};
+    border-radius: ${props => (props.$variant === "icon" ? "50%" : "6px")};
     transition: all 0.5s;
 
     &:hover {
         // 밝기 조절
         filter: brightness(0.8);
-        background-color: ${props => (props.$variant === "contained" ? undefined: props.theme.colors.background.default)};
+        background-color: ${props =>
+            props.$variant === "contained" ? undefined : props.theme.colors.background.default};
     }
 `;
 
@@ -24,13 +31,14 @@ interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
     children: ReactNode;
     color: ButtonColorType;
     variant: VariantType;
+    fullwidth: boolean;
     as?: ElementType;
     to?: string;
 };
 
-function Button({children, color, variant, ...props}: Props) {
+function Button({children, color, variant, fullwidth, ...props}: Props) {
     return (
-        <StyledButton $color={color} $variant={variant} {...(props as any)}>
+        <StyledButton $color={color} $variant={variant} $fullwidth={fullwidth} {...(props as any)}>
             {children}
         </StyledButton>
     );
